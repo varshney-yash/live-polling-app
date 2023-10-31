4e4beaf19f3ae78910109641288fbeb6c1b5e323
py3-dev <[200~yash.varshney@careers360.in>
2023-10-31 09:53:08 +0530

    deploy with verceL

--- db.py
+++ db.py
@@ -7,8 +7,8 @@ load_dotenv()
 
 import os 
 
-username = os.environ.get('mongo-username')
-passw = os.environ.get('mongo-password')
+username = os.environ.get('mongo_username')
+passw = os.environ.get('mongo_password')
 
 
 username = urllib.parse.quote_plus(username)
